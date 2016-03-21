3518a10bba54f0dfe345354471f42066a1317a8e
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-03-21 17:34:06 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, location)
+
+  if board[location] == "X" || board[location] == "O"
+    return true
+    elsif board[location] != " " || board[location] != "" || board[location] != nil
+    return false
+  end
+
+end
